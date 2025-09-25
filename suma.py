88fcf765bf162fdcf604f00f13b69e4ad0e44f31
JuanOsorio <jm.osorioo.98@gmail.com>
2025-09-25 17:43:08 -0600

    hice programa de suma

---
+++ suma.py
@@ -0,0 +1,5 @@
+# Programa que recibe dos números y muestra la suma
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+resultado = num1 + num2
+print("La suma es:", resultado)
